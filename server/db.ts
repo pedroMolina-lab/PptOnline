2edddb683bc9662b4f96ee492044be35f4655bd0
piter <pedri_molina@hotmail.com>
2023-07-13 21:22:04 -0300

    nuevoCommit

--- server/db.ts
+++ server/db.ts
@@ -1,17 +1,13 @@
-import * as dotenv from 'dotenv';         
-const admin = require("firebase-admin");
-dotenv.config()
-const key_json = process.env.KEY_JSON
-
+import * as admin from "firebase-admin"
+import * as serviceAccount from "./key.json"
 
 
 admin.initializeApp({
-    credential: admin.credential.cert(key_json),
+    credential: admin.credential.cert(serviceAccount as any),
     databaseURL: "https://dwf-modulo6-default-rtdb.firebaseio.com"
  });
 
- const firestore = admin.firestore()
+const firestore = admin.firestore()
 const rtdb = admin.database()
-
 
 export {firestore, rtdb}
