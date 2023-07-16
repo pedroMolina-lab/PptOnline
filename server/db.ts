ecda23265838c4b9f3a75d0edb9706faf36398b4
piter <pedri_molina@hotmail.com>
2023-07-16 12:19:53 -0300

    otroNuevoCommit

--- server/db.ts
+++ server/db.ts
@@ -1,19 +1,13 @@
-import * as admin from "firebase-admin";
-import * as dotenv from "dotenv";
-import path from "path";
-
-dotenv.config({path: "./.env"});
-
-
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+import * as admin from "firebase-admin"
+import * as serviceAccount from "./key.json"
 
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as any),
-  databaseURL: "https://dwf-modulo6-default-rtdb.firebaseio.com"
-});
+    credential: admin.credential.cert(serviceAccount as any),
+    databaseURL: "https://dwf-modulo6-default-rtdb.firebaseio.com"
+ });
 
-const firestore = admin.firestore();
-const rtdb = admin.database();
+const firestore = admin.firestore()
+const rtdb = admin.database()
 
-export { firestore, rtdb };
+export {firestore, rtdb}
