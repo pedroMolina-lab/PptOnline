1ee70c51da3c0fb0687091979f0fad787fe6202a
piter <pedri_molina@hotmail.com>
2023-07-15 23:16:39 -0300

    nuevosCommit

--- server/db.ts
+++ server/db.ts
@@ -1,23 +1,15 @@
 import * as admin from "firebase-admin";
 import * as dotenv from "dotenv";
+import path from "path";
 
-dotenv.config();
+dotenv.config({path: "./.env"});
 
-const key = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
 
-if (!key) {
-  throw new Error("La variable de entorno FIREBASE_SERVICE_ACCOUNT_KEY no está definida.");
-}
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
 
-let serviceAccount;
-try {
-  serviceAccount = JSON.parse(key);
-} catch (error) {
-  throw new Error("El contenido de FIREBASE_SERVICE_ACCOUNT_KEY no es un JSON válido.");
-}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount as any),
   databaseURL: "https://dwf-modulo6-default-rtdb.firebaseio.com"
 });
 
